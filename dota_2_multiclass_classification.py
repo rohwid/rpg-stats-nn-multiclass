5efe22a077523f5697802d87ff1dd734314a06b7
Rohman Widiyanto <widiyanto.rohman@gmail.com>
2021-01-07 00:52:07 +0700

    add README.MD, tensorboard directory

--- dota_2_multiclass_classification.py
+++ dota_2_multiclass_classification.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import tensorflow as tf
 
 from keras.models import Model, Sequential
 from keras.layers import Input, Activation, Dense
